30463ce4cf36d524753ce8d0d0986ab4532fac8a
Dan Ott <thedanielott@gmail.com>
2017-12-03 21:13:30 -0500

    Clean up the day 3 solution as much as seems worth it

--- day_3.rb
+++ day_3.rb
@@ -49,12 +49,34 @@ class TheTest < Minitest::Test
   end
 
   def test_part_2
-    skip
-    assert_equal
+    assert_equal 1, step_to_position(1)
+    assert_equal 1, step_to_position(2)
+    assert_equal 2, step_to_position(3)
+    assert_equal 4, step_to_position(4)
+    assert_equal 5, step_to_position(5)
+    assert_equal 10, step_to_position(6)
+    assert_equal 11, step_to_position(7)
+    assert_equal 23, step_to_position(8)
+    assert_equal 25, step_to_position(9)
+    assert_equal 26, step_to_position(10)
+    assert_equal 54, step_to_position(11)
+    assert_equal 57, step_to_position(12)
+    assert_equal 59, step_to_position(13)
+    assert_equal 122, step_to_position(14)
+    assert_equal 133, step_to_position(15)
+    assert_equal 142, step_to_position(16)
+    assert_equal 147, step_to_position(17)
+    assert_equal 304, step_to_position(18)
+    assert_equal 330, step_to_position(19)
+    assert_equal 351, step_to_position(20)
+    assert_equal 362, step_to_position(21)
+    assert_equal 747, step_to_position(22)
+    assert_equal 806, step_to_position(23)
   end
 
   def test_refactoring
     assert_equal 326, steps_required(361527)
+    assert_equal 363010, find_first_value_over_threshold(361527)
   end
 end
 
@@ -79,7 +101,7 @@ def steps_required(starting_position)
 end
 
 class Grid
-  attr_accessor :coordinates
+  attr_reader :coordinates
 
   def initialize(coordinates: [])
     @coordinates = coordinates
@@ -89,8 +111,27 @@ class Grid
     coordinate_at(x: x, y: y).value
   end
 
+  def value_of_neighbors(x:, y:)
+    NEIGHBORS.reduce(0) { |m, n| m + value_at(x: x + n.x, y: y + n.y) }
+  end
+
+  def values
+    coordinates.map(&:value)
+  end
+
   def coordinate_at(x:, y:)
     coordinates.find { |c| c.x == x && c.y == y } || Coordinate.new(x: x, y: y, value: 0)
+  end
+
+  def next_grid(heading)
+    next_x = coordinates.last.x + heading.x
+    next_y = coordinates.last.y + heading.y
+    next_coordinate = Coordinate.new(
+      x: next_x,
+      y: next_y,
+      value: value_of_neighbors(x: next_x, y: next_y),
+    )
+    self.class.new(coordinates: coordinates + [next_coordinate])
   end
 end
 
@@ -126,51 +167,34 @@ NEIGHBORS = [EAST, NORTH_EAST, NORTH, NORTH_WEST, WEST, SOUTH_WEST, SOUTH, SOUTH
 EAST_NORTH_WEST_SOUTH = [EAST, NORTH, WEST, SOUTH]
 
 def find_first_value_over_threshold(threshold)
-  directions = Enumerator.new do |yielder|
-    dir = EAST
-    loop do
-      yielder << dir
-      dir = case dir
-            when EAST
-              NORTH
-            when NORTH
-              WEST
-            when WEST
-              SOUTH
-            when SOUTH
-              EAST
-            end
-    end
+  1.upto(1_000_000) do |i|
+    value = step_to_position(i)
+    break value if value > threshold
   end
+end
 
+def step_to_position(position)
   grid = Grid.new(coordinates: [Coordinate.new(x: 0, y: 0, value: 1)])
-  paces = 0
+  headings = EAST_NORTH_WEST_SOUTH.cycle
+  extend_total_paces_on_heading_change = [true, false].cycle
 
-  while grid.coordinates.last.value < threshold
-    paces = paces.next
-    2.times do
-      direction = directions.next
-      paces.times do
-        next_position = Heading.new(
-          x: grid.coordinates.last.x + direction.x,
-          y: grid.coordinates.last.y + direction.y,
-        )
-        next_value = NEIGHBORS.reduce(0) do |memo, h|
-          memo + grid.value_at(
-            x: next_position.x + h.x,
-            y: next_position.y + h.y,
-           )
-        end
-        next_coordinate = Coordinate.new(
-          x: next_position.x,
-          y: next_position.y,
-          value: next_value,
-        )
-        grid = Grid.new(coordinates: grid.coordinates + [next_coordinate])
-        return grid.coordinates.last.value if grid.coordinates.last.value > threshold
-      end
+  heading = SOUTH
+  total_paces_in_this_heading = 0
+  paces_remaining_in_this_heading = 0
+
+  (position - 1).times do
+    if paces_remaining_in_this_heading.zero?
+      heading = headings.next
+      total_paces_in_this_heading = total_paces_in_this_heading.next if extend_total_paces_on_heading_change.next
+      paces_remaining_in_this_heading = total_paces_in_this_heading
     end
+
+    paces_remaining_in_this_heading -= 1
+
+    grid = grid.next_grid(heading)
   end
+
+  grid.values.last
 end
 
 result_part_1 = steps_required(361527)
